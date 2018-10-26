66b692470ebd9dab1d3e7f2059a2fdaa100a4bd0
drom <alex.drom@gmail.com>
2018-10-26 16:30:18 -0700

    play around

--- lzc/LZC.scala
+++ lzc/LZC.scala
@@ -3,13 +3,17 @@ import chisel3._
 
 class LZC(w: Int) extends Module {
   val io = IO(new Bundle {
-    val inp = Input(UInt(w.W))
-    val oup = Output(UInt(w.W))
+    val inp = Input(Vec(w, Bool()))
+    val oup = Output(UInt(8.W))
   })
-  io.oup := io.inp
+  io.oup := 255.U
+  for (i <- (w - 1) to 0 by -1) {
+    when (io.inp(i)) {
+      io.oup := i.U
+    }
+  }
 }
 
 object LZC extends App {
   chisel3.Driver.execute(args, () => new LZC(args(0).toInt))
 }
-
