02c59b02dbd5ae733947dcdc1cc4eeebc2757967
drom <alex.drom@gmail.com>
2018-10-30 18:36:29 -0700

    better LZC example

--- lzc/LZC.scala
+++ lzc/LZC.scala
@@ -1,64 +1,54 @@
 import chisel3._
-import scala.math._
-// import chisel3.util._
+import util._
 
-class LZCgen(w: Int) extends Module {
+class LZCgen(target: Vec[Bool]) extends Module {
+  val w = target.length
+  val log2w = log2max(w)
 
-  def log2max(x: Double): Int = ceil(log10(x) / log10(2.0)).toInt
-
-  val log2w = log2max(w);
+  val brick = (a: Vec[Bool], b: Vec[Bool]) => {
+    val len = a.length
+    val res = Wire(Vec(len + 1, Bool()))
+    for (i <- 0 to len - 1) {
+      res(i) := (if (i == 0) a(i) | b(i) else (~b(0) & a(i)) | b(i))
+    }
+    res(len) := b(0)
+    res
+  }
 
   val io = IO(new Bundle {
-    val inp = Input(Vec(w, Bool()))
-    val oup = Output(Vec(w, Bool()))
-    // val oup = Output(UInt(log2w.W))
+    val inp = Input(target)
+    val oup = Output(Vec(log2w + 1, Bool()))
   })
 
-  val tmp = io.inp.map { case a => a ^ true.B}
-  // Seq(true.B, false.B).foldLeft(io.inp) {
-  //   case (vec, b) => vec.map { case a => a ^ b }
-  // }
-  // var x = io.inp
-  // for(b <- Seq(true.B, false.B)) {
-  //   x = x.map { case a => a ^ b }
-  // }
-
-  io.oup := tmp
-  // io.oup := 1.U // -1 ???
-  //
-  // for (i <- (w - 1) to 0 by -1) {
-  //   when (io.inp(i)) {
-  //     io.oup := i.U
-  //   }
-  // }
-
-
+  var i = 0
+  val rec: Int => Vec[Bool] = (n: Int) => {
+    if (n == 0) {
+      i += 1
+      VecInit(io.inp(i - 1))
+    } else {
+      brick(rec(n - 1), rec(n - 1))
+    }
+  }
+  io.oup := rec(log2w)
 }
 
 object LZCgen {
   def apply(input: Vec[Bool]): Vec[Bool] = {
-    val mod = Module(new LZCgen(input.length))
+    val mod = Module(new LZCgen(chisel3.core.chiselTypeOf(input)))
     mod.io.inp := input
     mod.io.oup
   }
 }
 
 class LZC(w: Int) extends Module {
-
-  def log2max(x: Double): Int = ceil(log10(x) / log10(2.0)).toInt
-
-  val log2w = log2max(w);
-
+  val log2w = log2max(w)
   val io = IO(new Bundle {
     val inp = Input(Vec(w, Bool()))
-    val oup = Output(Vec(w, Bool()))
-    // val oup = Output(UInt(log2w.W))
+    val oup = Output(Vec(log2w + 1, Bool()))
   })
-
   io.oup := LZCgen(io.inp)
 }
 
 object LZC extends App {
-  //chisel3.Driver.execute(args, () => new LZC(args(0).toInt))
   println(chisel3.Driver.emitVerilog(new LZC(args(0).toInt)))
 }
